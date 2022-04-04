104932d3897b0472038a6f966edb70c8d564bca7
arif pambudi <arifpambudi242@gmail.com>
2022-04-04 21:38:02 +0700

    remove autocorrect spelling

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from os import system as cmd
 import time
 from googletrans import Translator
 import sys
-from autocorrect import Speller
-
-spell = Speller()
 
 trans = Translator(service_urls=['translate.google.com', 'translate.google.co.kr'])
 
@@ -28,16 +25,13 @@ for ind, para in enumerate(paragraphs):
 	if para.text:
 		inline = para.runs
 		for i, v in enumerate(inline):
-			# splelling correction
-			corrected = spell(v.text)
 			# translating
-			if corrected:
-				translated = trans.translate(corrected, dest=targetlang).text
+			translated = trans.translate(corrected, dest=targetlang).text
 			# set paragraph text
 			if translated:
 				v.text = translated
 	time.sleep(0.1)
-	cmd("clear") # change to cmd("cls") if your os is windows 
+	cmd("cls") # change to cmd("cls") if your os is windows 
 
 filename = filepath.split(".docx", 1)[0]
 document.save(f"{filename}-{targetlang}-{round(time.time())}.docx")
